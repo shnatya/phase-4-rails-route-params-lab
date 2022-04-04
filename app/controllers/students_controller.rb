eaedf3ab395d87c4a854ab24a43a75a319c4eb92
Anastasia Shatokhina <shnatya@gmail.com>
2022-04-04 08:44:31 -0700

    commit tree 63db0b4be24929b1df4e231f8648827ed3e11ef8

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name]
+      students = Student.where("first_name = ? OR last_name = ?", params[:name].capitalize, params[:name].capitalize)
+    else
+      
+      students = Student.all
+    end
     render json: students
   end
 
+  def show
+    student = Student.find_by_id(params[:id])
+    if student
+      render json: student
+    else
+      render json: { error: 'Student not found' }, status: 404
+    end
+  end
 end
